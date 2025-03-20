6396c056f9311388aa4b48512f824c79ab1f215e
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2025-03-20 11:51:10 -0600

    first commit

---
+++ src/auto-plant-watering-system.cpp
@@ -0,0 +1,20 @@
+/* 
+ * Project: Automatic Plant Watering System
+ * Author: Marlon McPherson
+ * Date: 20 MAR 2025
+ */
+
+#include "Particle.h"
+#include "credentials.h"
+
+SYSTEM_MODE(SEMI_AUTOMATIC);
+// SYSTEM_THREAD(ENABLED);
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
