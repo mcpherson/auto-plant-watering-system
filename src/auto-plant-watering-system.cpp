fa27ebe80de7612f2849121e5d05bafdab9b6117
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2025-03-21 15:25:05 -0600

    pushing before commenting out I2C code while testing pump

--- src/auto-plant-watering-system.cpp
+++ src/auto-plant-watering-system.cpp
@@ -13,13 +13,14 @@
 #include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
 #include "Adafruit_MQTT/Adafruit_MQTT.h"
 
-SYSTEM_MODE(SEMI_AUTOMATIC);
+SYSTEM_MODE(AUTOMATIC);
 // SYSTEM_THREAD(ENABLED);
 SerialLogHandler logHandler(LOG_LEVEL_INFO);
 
 TCPClient TheClient; 
 Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY); 
 
+Adafruit_MQTT_Subscribe buttonSubFeed = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/button-on-off"); 
 Adafruit_MQTT_Subscribe emailSubFeed = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/email"); 
 Adafruit_MQTT_Publish temperaturePubFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/midterm2-temperature");
 Adafruit_MQTT_Publish humidityPubFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/midterm2-humidity");
@@ -38,12 +39,30 @@ const int OLED_RESET=-1;
 Adafruit_SSD1306 display(OLED_RESET);
 
 const int MOISTPIN = A0;
-const int DUSTPIN = A2;
+const int PUMPPIN = D16;
+// const int DUSTPIN = A2;
 
 unsigned int lastTime;
+int buttonValue;
+int moistMid = 2500;
+int dryness, dryMagnitude, moisture;
+String moistureState;
+String dateTime, m, d, timeOnly, yr;
 
+// dust sensor
+// unsigned long duration;
+// unsigned long dustStartTime;
+// unsigned long dustSampleTime = 30000;
+// unsigned long lowPulseOccupancy = 0;
+// float ratio = 0;
+float concentration = 0;
+
+String getTime();
 void bmeRead();
-void bmeDisplay();
+void getMoisture();
+void getDustLevel();
+void displayData(String timestamp);
+void publishData();
 void MQTT_connect();
 bool MQTT_ping();
 
@@ -51,13 +70,19 @@ void setup() {
   Serial.begin(9600);
   waitFor(Serial.isConnected, 10000);
 
-  // Connect to Internet but not Particle Cloud
   WiFi.on();
   WiFi.connect();
   while(WiFi.connecting()) {
     Serial.printf(".");
   }
   Serial.printf("\n\n");
+
+  // Setup MQTT subscription
+  // mqtt.subscribe(&emailSubFeed);
+  mqtt.subscribe(&buttonSubFeed);
+
+  Time.zone(-6);
+  Particle.syncTime();
 
   status = BME.begin(BME_ADDRESS);
   if (status == false) {
@@ -66,23 +91,73 @@ void setup() {
 
   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS);
   display.clearDisplay();
+
+  pinMode(MOISTPIN, INPUT);
+  pinMode(PUMPPIN, OUTPUT);
+
+  // pinMode(DUSTPIN, INPUT);
+  // dustStartTime = millis();
 }
 
 void loop() {
   MQTT_connect();
   MQTT_ping();
 
+  // this is our 'wait for incoming subscription packets' busy subloop
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(100))) {
+    if (subscription == &buttonSubFeed) {
+      buttonValue = atoi((char *)buttonSubFeed.lastread);
+      Serial.printf("Toggle: %i\n", buttonValue);
+      if (buttonValue == 1) {
+        Serial.printf("ON\n");
+        digitalWrite(PUMPPIN, HIGH);
+        delay(500);
+      }
+      else {
+        Serial.printf("OFF\n");
+        digitalWrite(PUMPPIN, LOW);
+      }
+    }
+    // if (subscription == &brightnessSubFeed) {
+    //   brightnessValue = atoi((char *)brightnessSubFeed.lastread);
+    //   Serial.printf("Setting brightness to %i\n", brightnessValue);
+    //   analogWrite(LEDPIN2, brightnessValue);
+    // }
+  }
+
+  // getDustLevel();
+
   // publish to Adafruit.io every 10 seconds
   if((millis() - lastTime > 10000)) {
     if(mqtt.Update()) {
+      String timestamp = getTime();
       bmeRead();
-      temperaturePubFeed.publish(BMEData[0]);
-      pressurePubFeed.publish(BMEData[1]);
-      humidityPubFeed.publish(BMEData[2]);
-      // moisturePubFeed.publish(moisture);
+      getMoisture();
+      displayData(timestamp);
+      publishData();
+      Serial.printf("DRY: %i, WET: %i\n", dryness, moisture);
       } 
     lastTime = millis();
   }
+}
+
+String getTime() {
+  String shortTime;
+  dateTime = Time.timeStr();
+  m = dateTime.substring(4, 7).toUpperCase();
+  d = dateTime.substring(8, 10);
+  // h:m
+  timeOnly = dateTime.substring(11, 16);
+  yr = dateTime.substring(20, 24);
+  shortTime.concat(d);
+  shortTime.concat(" ");
+  shortTime.concat(m);
+  shortTime.concat(" ");
+  shortTime.concat(yr);
+  shortTime.concat(" ");
+  shortTime.concat(timeOnly);
+  return shortTime;
 }
 
 void bmeRead() {
@@ -92,16 +167,68 @@ void bmeRead() {
   BMEData[1] = BME.readPressure() / 1000;
   // humidity (%RH)
   BMEData[2] = BME.readHumidity();
-  bmeDisplay();
 }
 
-void bmeDisplay() {
+void displayData(String timestamp) {
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.printf("Temp:     %0.2f %cF\nHumidity: %0.2f %%RH\nPressure: %0.2f kPa", BMEData[0], degreeSymbol, BMEData[2], BMEData[1]);
+  display.printf("  %s\n\nTemp:      %0.2f %cF\nHumidity:  %0.2f %%RH\nPressure:  %0.2f kPa\nDust:      %0.0fcf\nMoisture:  %s", timestamp.c_str(), BMEData[0], degreeSymbol, BMEData[2], BMEData[1], concentration, moistureState.c_str());
   display.display();
+}
+
+// get reading from moisture sensor and convert to a value that represents moisture, not dryness.
+void getMoisture() {
+  dryness = analogRead(MOISTPIN);
+  dryMagnitude = abs(moistMid - dryness);
+  dryness > moistMid 
+  ? moisture = moistMid - dryMagnitude 
+  : moisture = moistMid + dryMagnitude;
+  // NOTE: not all c++ compilers can handle ranges in switch cases.
+  switch (moisture) {
+    case 3400 ... 4000:
+      moistureState = "SATURATED";
+      break;
+    case 3000 ... 3399:
+      moistureState = "WET";
+      break;
+    case 2500 ... 2999:
+      moistureState = "MOIST";
+      break;
+    case 2000 ... 2499:
+      moistureState = "DAMP";
+      break;
+    case 1600 ... 1999:
+      moistureState = "PARCHED";
+      break;
+    case 0 ... 1599:
+      moistureState = "DRY";
+      break;
+    default:
+      break;
+  }
+}
+
+// void getDustLevel() {
+//   duration = pulseIn(DUSTPIN, LOW);
+//   lowPulseOccupancy = lowPulseOccupancy + duration;
+  
+//   if ((millis() - dustStartTime) > dustSampleTime) {
+//     ratio = lowPulseOccupancy / (dustSampleTime * 10.0);
+//     // using spec sheet curve
+//     concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; 
+//     Serial.printf("DUST - LPO: %i, Ratio: %0.3f, Concentration: %0.3f\n", lowPulseOccupancy, ratio, concentration);
+//     lowPulseOccupancy = 0;
+//     dustStartTime = millis();
+//   }
+// }
+
+void publishData() {
+  temperaturePubFeed.publish(BMEData[0]);
+  pressurePubFeed.publish(BMEData[1]);
+  humidityPubFeed.publish(BMEData[2]);
+  moisturePubFeed.publish(moisture);
 }
 
 // Function to connect and reconnect as necessary to the MQTT server.
